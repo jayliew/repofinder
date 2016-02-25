6bd4388c7f32848afa00abec0531d8220d3ff071
Jay Liew <jayliew@jayliew.com>
2016-02-25 13:44:54 -0800

    autolayout not working

--- GithubDemo/RepoResultsViewController.swift
+++ GithubDemo/RepoResultsViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 import MBProgressHUD
 
 // Main ViewController
-class RepoResultsViewController: UIViewController {
+class RepoResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    // MARK: Properties
+    
     var searchBar: UISearchBar!
     var searchSettings = GithubRepoSearchSettings()
-
     var repos: [GithubRepo]!
-
+    @IBOutlet weak var reposTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        reposTableView.dataSource = self
+        reposTableView.delegate = self
+        reposTableView.estimatedRowHeight = 300
+        reposTableView.rowHeight = UITableViewAutomaticDimension
+        
         // Initialize the UISearchBar
         searchBar = UISearchBar()
         searchBar.delegate = self
@@ -31,6 +38,34 @@ class RepoResultsViewController: UIViewController {
         // Perform the first search when the view controller first loads
         doSearch()
     }
+    
+    // MARK: UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let repos = self.repos {
+            return repos.count
+        }else{
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("com.jayliew.ReposViewCell", forIndexPath: indexPath) as! ReposViewCell
+        let repo = self.repos[indexPath.row]
+        
+        cell.avatarImageView.setImageWithURL(NSURL(string: repo.ownerAvatarURL!)!)
+        cell.nameLabel.text = repo.name
+        
+        cell.descriptionLabel.text = repo.repoDescription
+//        cell.descriptionLabel.sizeToFit()
+//        cell.descriptionLabel.numberOfLines = 0
+        
+        cell.ownerLabel.text = repo.ownerHandle
+        cell.forksLabel.text = String(repo.forks)
+        cell.starsLabel.text = String(repo.stars)
+        
+        return cell
+    }
 
     // Perform the search.
     private func doSearch() {
@@ -40,14 +75,16 @@ class RepoResultsViewController: UIViewController {
         // Perform request to GitHub API to get the list of repositories
         GithubRepo.fetchRepos(searchSettings, successCallback: { (newRepos) -> Void in
 
-            
             self.repos = [GithubRepo]()
+            
             // Print the returned repositories to the output window
             for repo in newRepos {
                 print(repo)
                 self.repos.append(repo)
             }   
 
+            self.reposTableView.reloadData()
+            
             MBProgressHUD.hideHUDForView(self.view, animated: true)
             }, error: { (error) -> Void in
                 print(error)
---
+++ GithubDemo/ReposViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  ReposViewCell.swift
+//  GithubDemo
+//
+//  Created by Jay Liew on 2/24/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ReposViewCell: UITableViewCell {
+
+    // MARK: Properties
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var forksImageView: UIImageView!
+    @IBOutlet weak var starsImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var forksLabel: UILabel!
+    @IBOutlet weak var starsLabel: UILabel!
+    @IBOutlet weak var ownerLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
