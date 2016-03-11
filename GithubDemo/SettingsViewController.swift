738d7c118d2e5d2256b3486fcec87a78bc11bdb7
Jay Liew <jayliew@jayliew.com>
2016-03-10 17:40:33 -0800

    slider works on settings page

--- GithubDemo/SettingsViewController.swift
+++ GithubDemo/SettingsViewController.swift
@@ -10,21 +10,31 @@ import UIKit
 
 class SettingsViewController: UITableViewController {
 
+    // MARK: Outlets
+    
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var starSlider: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sliderValueChanged(sender: UISlider) {
+        let currentValue = Int(sender.value)
+        sliderLabel.text = String(currentValue)
     }
     
     @IBAction func cancelToReposResultsViewController(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
-    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
     /*
     // MARK: - Navigation
 
